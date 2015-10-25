d37544885d2c8f437a0cc9e43c6b8f34bca6d77e
Andrew Lord <andrew@lordcodes.com>
2015-10-25 13:24:14 +0000

    Added tests for SnackbarCallback.

--- snackbarbuilder/src/main/java/com/andrewlord/snackbarbuilder/SnackbarCallback.java
+++ snackbarbuilder/src/main/java/com/andrewlord/snackbarbuilder/SnackbarCallback.java
@@ -8,27 +8,27 @@ public class SnackbarCallback {
     private static final String TAG = SnackbarCallback.class.getSimpleName();
 
     public void onSnackbarShown(Snackbar snackbar) {
-        Log.v(TAG, "onSnackbarShown " + snackbar.toString());
+        Log.v(TAG, "onSnackbarShown");
     }
 
     public void onSnackbarActionPressed(Snackbar snackbar) {
-        Log.v(TAG, "onSnackbarActionPressed " + snackbar.toString());
+        Log.v(TAG, "onSnackbarActionPressed");
     }
 
     public void onSnackbarSwiped(Snackbar snackbar) {
-        Log.v(TAG, "onSnackbarSwiped " + snackbar.toString());
+        Log.v(TAG, "onSnackbarSwiped");
     }
 
     public void onSnackbarTimedOut(Snackbar snackbar) {
-        Log.v(TAG, "onSnackbarTimedOut " + snackbar.toString());
+        Log.v(TAG, "onSnackbarTimedOut");
     }
 
     public void onSnackbarManuallyDismissed(Snackbar snackbar) {
-        Log.v(TAG, "onSnackbarManuallyDismissed " + snackbar.toString());
+        Log.v(TAG, "onSnackbarManuallyDismissed");
     }
 
     public void onSnackbarDismissedAfterAnotherShown(Snackbar snackbar) {
-        Log.v(TAG, "onSnackbarDismissedAfterAnotherShown " + snackbar.toString());
+        Log.v(TAG, "onSnackbarDismissedAfterAnotherShown");
     }
 
 }
---
+++ snackbarbuilder/src/test/java/com/andrewlord/snackbarbuilder/SnackbarCallbackTest.java
@@ -0,0 +1,99 @@
+package com.andrewlord.snackbarbuilder;
+
+import android.support.design.widget.Snackbar;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.shadows.ShadowLog;
+import org.robolectric.shadows.ShadowLog.LogItem;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class SnackbarCallbackTest {
+
+    private SnackbarCallback callbackUnderTest;
+
+    @Mock
+    Snackbar snackbar;
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+
+        callbackUnderTest = new SnackbarCallback();
+    }
+
+    @Test
+    public void whenOnSnackbarShown_thenSnackbarShownMessageLogged() {
+        //When
+        callbackUnderTest.onSnackbarShown(snackbar);
+
+        //Then
+        List<LogItem> logs = ShadowLog.getLogsForTag(SnackbarCallback.class.getSimpleName());
+        assertThat(logs).hasSize(1);
+        assertThat(logs.get(0).msg).isEqualTo("onSnackbarShown");
+    }
+
+    @Test
+    public void whenOnSnackbarActionPressed_thenSnackbarActionPressedMessageLogged() {
+        //When
+        callbackUnderTest.onSnackbarActionPressed(snackbar);
+
+        //Then
+        List<LogItem> logs = ShadowLog.getLogsForTag(SnackbarCallback.class.getSimpleName());
+        assertThat(logs).hasSize(1);
+        assertThat(logs.get(0).msg).isEqualTo("onSnackbarActionPressed");
+    }
+
+    @Test
+    public void whenOnSnackbarSwiped_thenSnackbarSwipedMessageLogged() {
+        //When
+        callbackUnderTest.onSnackbarSwiped(snackbar);
+
+        //Then
+        List<LogItem> logs = ShadowLog.getLogsForTag(SnackbarCallback.class.getSimpleName());
+        assertThat(logs).hasSize(1);
+        assertThat(logs.get(0).msg).isEqualTo("onSnackbarSwiped");
+    }
+
+    @Test
+    public void whenOnSnackbarTimedOut_thenSnackbarTimedOutMessageLogged() {
+        //When
+        callbackUnderTest.onSnackbarTimedOut(snackbar);
+
+        //Then
+        List<LogItem> logs = ShadowLog.getLogsForTag(SnackbarCallback.class.getSimpleName());
+        assertThat(logs).hasSize(1);
+        assertThat(logs.get(0).msg).isEqualTo("onSnackbarTimedOut");
+    }
+
+    @Test
+    public void whenOnSnackbarManuallyDismissed_thenSnackbarManuallyDismissedMessageLogged() {
+        //When
+        callbackUnderTest.onSnackbarManuallyDismissed(snackbar);
+
+        //Then
+        List<LogItem> logs = ShadowLog.getLogsForTag(SnackbarCallback.class.getSimpleName());
+        assertThat(logs).hasSize(1);
+        assertThat(logs.get(0).msg).isEqualTo("onSnackbarManuallyDismissed");
+    }
+
+    @Test
+    public void whenOnSnackbarDismissedAfterAnotherShown_thenSnackbarDismissedAfterAnotherShownMessageLogged() {
+        //When
+        callbackUnderTest.onSnackbarDismissedAfterAnotherShown(snackbar);
+
+        //Then
+        List<LogItem> logs = ShadowLog.getLogsForTag(SnackbarCallback.class.getSimpleName());
+        assertThat(logs).hasSize(1);
+        assertThat(logs.get(0).msg).isEqualTo("onSnackbarDismissedAfterAnotherShown");
+    }
+
+}
