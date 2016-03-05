4dca13e76845ba7eab93c2507b5a71b379f3f67e
Andrew Lord <andrew@lordcodes.com>
2016-03-05 19:37:03 +0000

    Added JavaDoc comments for the public API of ToastBuilder.

--- snackbarbuilder/src/main/java/com/github/andrewlord1990/toastbuilder/ToastBuilder.java
+++ snackbarbuilder/src/main/java/com/github/andrewlord1990/toastbuilder/ToastBuilder.java
@@ -15,6 +15,7 @@ package com.github.andrewlord1990.toastbuilder;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar.Duration;
@@ -25,80 +26,160 @@ import android.widget.Toast;
 
 import com.github.andrewlord1990.snackbarbuilder.R;
 
+/**
+ * A builder pattern to easily create Toasts. On top of the options
+ * provided through the platform API, you can also change the text
+ * colour to display the message.
+ */
 public class ToastBuilder {
 
     Context context;
-
     CharSequence message;
-
     View customView;
-
     int customViewMessageViewId = android.R.id.message;
+    int messageTextColor;
 
     @Duration
     int duration = Toast.LENGTH_LONG;
-
-    int messageTextColor;
 
     int gravity;
     int gravityOffsetX;
     int gravityOffsetY;
 
+    /**
+     * Create a builder to setup and then create a {@link Toast}.
+     *
+     * @param context The context to use for creating the Toast.
+     */
     public ToastBuilder(Context context) {
         this.context = context;
         loadThemeAttributes();
     }
 
+    /**
+     * Set a custom view to apply to the {@link Toast} message.
+     *
+     * @param customView The custom view.
+     * @return This instance.
+     */
     public ToastBuilder customView(View customView) {
         this.customView = customView;
         return this;
     }
 
+    /**
+     * Set the ID of a TextView within the custom view to
+     * set the message on.
+     *
+     * @param customViewMessageViewId The ID of the message
+     *                                TextView within the
+     *                                custom view.
+     * @return This instance.
+     */
     public ToastBuilder customViewMessageViewId(int customViewMessageViewId) {
         this.customViewMessageViewId = customViewMessageViewId;
         return this;
     }
 
+    /**
+     * Set the message to display on the {@link Toast}.
+     *
+     * @param message The text to display.
+     * @return This instance.
+     */
     public ToastBuilder message(CharSequence message) {
         this.message = message;
         return this;
     }
 
+    /**
+     * Set the message to display on the {@link Toast}.
+     *
+     * @param messageResId The string resource of the text to display.
+     * @return This instance.
+     */
     public ToastBuilder message(@StringRes int messageResId) {
         this.message = context.getString(messageResId);
         return this;
     }
 
-    public ToastBuilder messageTextColorRes(@ColorRes int messageTextColor) {
-        this.messageTextColor = getColor(messageTextColor);
-        return this;
-    }
-
-    public ToastBuilder messageTextColor(int messageTextColor) {
+    /**
+     * Set the color to display the message on the {@link Toast}.
+     *
+     * @param messageTextColor The color to display the message.
+     * @return This instance.
+     */
+    public ToastBuilder messageTextColor(@ColorInt int messageTextColor) {
         this.messageTextColor = messageTextColor;
         return this;
     }
 
+    /**
+     * Set the color to display the message on the {@link Toast}.
+     *
+     * @param messageTextColorResId The resource of the color to
+     *                              display the message.
+     * @return This instance.
+     */
+    public ToastBuilder messageTextColorRes(@ColorRes int messageTextColorResId) {
+        this.messageTextColor = getColor(messageTextColorResId);
+        return this;
+    }
+
+    /**
+     * Set the duration to display the {@link Toast} for.
+     *
+     * @param duration Duration which can be either LENGTH_SHORT
+     *                 or LENGTH_LONG.
+     * @return This instance.
+     */
     public ToastBuilder duration(@Duration int duration) {
         this.duration = duration;
         return this;
     }
 
+    /**
+     * Set the location the {@link Toast} appears on the screen. The
+     * gravity specifies which edge of the screen it is attached to.
+     *
+     * @param gravity The location to display the Toast.
+     * @return This instance.
+     */
     public ToastBuilder gravity(int gravity) {
         this.gravity = gravity;
         return this;
     }
 
+    /**
+     * Set the horizontal offset in pixels to apply to the
+     * {@link Toast} location.
+     *
+     * @param gravityOffsetX The horizontal offset in pixels.
+     * @return This instance.
+     */
     public ToastBuilder gravityOffsetX(int gravityOffsetX) {
         this.gravityOffsetX = gravityOffsetX;
         return this;
     }
 
+    /**
+     * Set the vertical offset in pixels to apply to the
+     * {@link Toast} location.
+     *
+     * @param gravityOffsetY The vertical offset in pixels.
+     * @return This instance.
+     */
     public ToastBuilder gravityOffsetY(int gravityOffsetY) {
         this.gravityOffsetY = gravityOffsetY;
         return this;
     }
 
+    /**
+     * Build a {@link Toast} using the options specified in
+     * the builder.
+     *
+     * @return The constructed Toast.
+     */
     @SuppressLint("ShowToast")
     public Toast build() {
         Toast toast = Toast.makeText(context, message, duration);
